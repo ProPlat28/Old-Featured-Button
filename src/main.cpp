9520683d5b0a36049d74adbc8e313f0cea83e230
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-12 19:00:29 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
 
 using namespace geode::prelude;
 
-constexpr float FEATURED_BTN_SCALE_MULTIPLIER = 0.9f;
+constexpr float FEATURED_BTN_SCALE_MULTIPLIER = 1.1f;
 
 class $modify(OldFeaturedHook, CreatorLayer) {
     bool init() {
