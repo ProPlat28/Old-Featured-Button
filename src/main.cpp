ad6c857c362bec5cccd58f08a7eaaeeff92ea0d8
ProPlat28 <johnwickthebest96@gmail.com>
2026-09-13 13:01:53 +0200

    Refactor featured button handling in CreatorLayer
    Refactor featured button handling in CreatorLayer. Added methods to swap button textures and find sibling button sizes.

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,97 @@
 #include <Geode/Geode.hpp>
 #include <Geode/modify/CreatorLayer.hpp>
 #include <Geode/binding/CCMenuItemSpriteExtra.hpp>
+#include <string>
+#include <algorithm>
 
 using namespace geode::prelude;
 
-class $modify(CreatorLayer) {
-    bool init() {
-        if (!CreatorLayer::init())
-            return false;
+constexpr float MyFeaturedButton = 1.0f;
 
-        findFeatured(this);
+class $modify(OldFeaturedHook, CreatorLayer) {
+    bool init() {
+        if (!CreatorLayer::init()) return false;
+
+        this->swapFeaturedButtonTexture(this);
+
         return true;
     }
 
-    void findFeaturedButton(CCNode* node) {
-    auto id = std::string(node->getID());
+    void swapFeaturedButtonTexture(CCNode* node) {
+        if (!node) return;
 
-    if (id.find("featured") != std::string::npos) {
-        auto button = typeinfo_cast<CCMenuItemSpriteExtra*>(node);
+        std::string const id = node->getID();
 
-        if (!button)
-            button = typeinfo_cast<CCMenuItemSpriteExtra*>(node->getParent());
-
-        if (button)
-            changeFeaturedButton(button);
-    }
+        if (id.find("featured") != std::string::npos) {
+            if (auto btn = typeinfo_cast<CCMenuItemSpriteExtra*>(node)) {
+                this->replaceButtonSprite(btn);
+            }
+            else if (auto parentBtn = typeinfo_cast<CCMenuItemSpriteExtra*>(node->getParent())) {
+                this->replaceButtonSprite(parentBtn);
+            }
+        }
 
         auto children = node->getChildren();
+        if (!children) return;
 
-        if (!children)
+        for (int i = 0; i < children->count(); i++) {
+            this->swapFeaturedButtonTexture(static_cast<CCNode*>(children->objectAtIndex(i)));
+        }
+    }
+
+    CCSize findSiblingButtonSize(CCMenuItemSpriteExtra* btn) {
+        auto parent = btn->getParent();
+        if (!parent) return CCSizeZero;
+
+        auto siblings = parent->getChildren();
+        if (!siblings) return CCSizeZero;
+
+        for (int i = 0; i < siblings->count(); i++) {
+            auto sibling = typeinfo_cast<CCMenuItemSpriteExtra*>(siblings->objectAtIndex(i));
+            if (sibling && sibling != btn) {
+                auto size = sibling->getContentSize();
+                if (size.width > 0.f && size.height > 0.f) {
+                    return size;
+                }
+            }
+        }
+
+        return CCSizeZero;
+    }
+
+    void replaceButtonSprite(CCMenuItemSpriteExtra* btn) {
+        CCSize targetSize = this->findSiblingButtonSize(btn);
+        if (targetSize.width <= 0.f || targetSize.height <= 0.f) {
+            targetSize = btn->getContentSize();
+        }
+
+        auto newSprite = CCSprite::create("old_featured_btn.png"_spr);
+        if (!newSprite) {
+            log::warn("OldFeaturedIcon: failed to load replacement sprite");
             return;
+        }
 
-        for (unsigned int i = 0; i < children->count(); i++) {
-            findFeatured(
-                static_cast<CCNode*>(children->objectAtIndex(i))
+        auto rawSize = newSprite->getContentSize();
+        float scale = 1.f;
+        if (rawSize.width > 0.f && rawSize.height > 0.f &&
+            targetSize.width > 0.f && targetSize.height > 0.f) {
+            scale = std::min(
+                targetSize.width / rawSize.width,
+                targetSize.height / rawSize.height
             );
         }
+        scale *= FEATURED_BTN_SCALE_MULTIPLIER;
+        newSprite->setScale(scale);
+
+        btn->setNormalImage(newSprite);
+
+        if (targetSize.width > 0.f && targetSize.height > 0.f) {
+            btn->setContentSize(CCSize(
+                rawSize.width * scale,
+                rawSize.height * scale
+            ));
+        }
+
+        log::info("OldFeaturedIcon: replaced Featured button sprite");
     }
 };
